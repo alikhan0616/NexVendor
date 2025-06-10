58ecc7eb233bbe52148e6b3f3bb36192e02e1b71
alikhan0616 <ak098boy@gmail.com>
2025-06-10 18:10:51 +0500

    make best selling page and product page dynamic aswell

--- frontend/src/pages/BestSellingPage.jsx
+++ frontend/src/pages/BestSellingPage.jsx
@@ -4,25 +4,34 @@ import styles from "../styles/styles";
 import { useEffect } from "react";
 import { productData } from "../static/data";
 import ProductCard from "../components/Route/ProductCard/ProductCard";
+import { useSelector } from "react-redux";
+import Loader from "../components/Layout/Loader";
 const BestSellingPage = () => {
   const [data, setData] = useState([]);
+  const { allProducts, isLoading } = useSelector((state) => state.product);
 
   useEffect(() => {
-    const d =
-      productData && productData.sort((a, b) => b.total_sell - a.total_sell);
+    const d = allProducts;
     setData(d);
-  }, []);
+  }, [allProducts]);
   return (
-    <div>
-      <Header activeHeading={2} />
-      <br />
-      <br />
-      <div className={`${styles.section} mt-10 800px:mt-0`}>
-        <div className="grid grid-cols-1 gap-5 md:grid-cols-2 md:gap-[25px] lg:grid-cols-4 lg:gap-[25px] xl:grid-cols-5 xl:gap-[30x] mb-12">
-          {data && data.map((i, index) => <ProductCard data={i} key={index} />)}
+    <>
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <div>
+          <Header activeHeading={2} />
+          <br />
+          <br />
+          <div className={`${styles.section} mt-10 800px:mt-0`}>
+            <div className="grid grid-cols-1 gap-5 md:grid-cols-2 md:gap-[25px] lg:grid-cols-4 lg:gap-[25px] xl:grid-cols-5 xl:gap-[30x] mb-12">
+              {data &&
+                data.map((i, index) => <ProductCard data={i} key={index} />)}
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
+      )}
+    </>
   );
 };
 
