348920f8ce2d822dd21e3ee98b2af60e2f549394
alikhan0616 <ak098boy@gmail.com>
2025-07-02 19:20:24 +0500

    minrot toast fix

--- frontend/src/components/Shop/ShopInfo.jsx
+++ frontend/src/components/Shop/ShopInfo.jsx
@@ -6,6 +6,7 @@ import { Link, useNavigate, useParams } from "react-router-dom";
 import Loader from "../Layout/Loader";
 import { useDispatch, useSelector } from "react-redux";
 import { getAllProductsShop } from "../../redux/actions/product";
+import { toast } from "react-toastify";
 
 const ShopInfo = ({ isOwner }) => {
   const [data, setData] = useState({});
