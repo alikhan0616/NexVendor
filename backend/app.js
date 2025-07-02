f27b931333d67f98e73ff70865c454802c106cff
alikhan0616 <ak098boy@gmail.com>
2025-07-02 14:57:35 +0500

    deployment pt.3

--- backend/app.js
+++ backend/app.js
@@ -20,7 +20,9 @@ const conversationRoute = require("./controller/conversation.js");
 const messageRoute = require("./controller/messages.js");
 const withdrawRoute = require("./controller/withdraw.js");
 const app = express();
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(
+  cors({ origin: "https://nex-vendor-ssk2.vercel.app/", credentials: true })
+);
 
 app.use(express.json({ limit: "50mb" }));
 app.use(cookieParser());
