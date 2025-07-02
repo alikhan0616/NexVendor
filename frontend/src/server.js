be38c26fa7c8da92aa722fa39cacf5216cc4fe46
alikhan0616 <ak098boy@gmail.com>
2025-07-02 15:59:35 +0500

    deployment pt-8

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,3 +1,2 @@
-export const server =
-  "https://nex-vendor-ak098boy-gmailcoms-projects.vercel.app/api/v2";
+export const server = "https://nexvendor.onrender.com/api/v2";
 export const backend_url = "http://localhost:8000/";
